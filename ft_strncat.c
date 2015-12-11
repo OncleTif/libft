4581ef3469ce2d4495e8ed1322d6cff1831d36ca
Tiphaine MANET <tmanet@e3r5p8.42.fr>
2015-12-11 10:39:58 +0100

    correction du \0

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 16:51:43 by tmanet            #+#    #+#             */
-/*   Updated: 2015/12/10 17:21:36 by tmanet           ###   ########.fr       */
+/*   Updated: 2015/12/11 10:20:09 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ char	*ft_strncat(char *s1, const char *s2, size_t n)
 
 	size = ft_strlen(s1);
 	ft_strncpy(s1 + size, s2, n);
-	s1[size + n + 1] = 0;
+	s1[size + n] = 0;
 	return (s1);
 }
