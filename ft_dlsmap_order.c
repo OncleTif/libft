21defb645a960f5ba31fb1babdbc47261ac40fee
tmanet <tmanet@student.42.fr>
2017-10-13 12:24:22 +0200

    dlsmap order ok

--- ft_dlsmap_order.c
+++ ft_dlsmap_order.c
@@ -6,27 +6,27 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/13 11:35:55 by tmanet            #+#    #+#             */
-/*   Updated: 2017/10/13 11:40:29 by tmanet           ###   ########.fr       */
+/*   Updated: 2017/10/13 12:15:42 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 t_dlst	*ft_dlsmap_order(void *ptr, t_dlst *(*f)(void **),
-		void *(fs)(t_dlst**, t_dlst*))
+		void (*fs)(t_dlst**, t_dlst*))
 {
-	t_dlst	*tmp;
-	t_dlst	*nxt;
+	t_dlst	*new;
+	t_dlst	*lst;
 
-	tmp = NULL;
+	lst = NULL;
 	if (ptr && f)
 	{
-		tmp = f(&ptr);
-		if (tmp)
+		new = f(&ptr);
+		if (new)
 		{
-			nxt = ft_dlsmap(ptr, f);
-			fs(&tmp, nxt);
+			lst = ft_dlsmap_order(ptr, f, fs);
+			fs(&lst, new);
 		}
 	}
-	return (tmp);
+	return (lst);
 }
