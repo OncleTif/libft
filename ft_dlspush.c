9fdefd7c6e52504970d8a58f2d6259af361e30a6
tmanet <tmanet@student.42.fr>
2017-10-13 15:30:48 +0200

    ft_dlspush ok

--- ft_dlspush.c
+++ ft_dlspush.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/11 11:49:24 by tmanet            #+#    #+#             */
-/*   Updated: 2017/10/11 11:51:40 by tmanet           ###   ########.fr       */
+/*   Updated: 2017/10/13 15:30:05 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	ft_dlspush(t_dlst **alst, t_dlst *new)
 	{
 		new->nxt = *alst;
 		if (new->nxt)
+		{
+			new->prv = new->nxt->prv;
 			new->nxt->prv = new;
+		}
 		*alst = new;
 	}
 }
