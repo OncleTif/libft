ad9eec3d90db56251817f50a872d131b13688546
tmanet <tmanet@student.42.fr>
2017-10-17 18:39:59 +0200

    dlsdelone debug et gestion prv et nxt

--- ft_dlsdelone.c
+++ ft_dlsdelone.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:52:34 by tmanet            #+#    #+#             */
-/*   Updated: 2017/10/13 12:42:37 by tmanet           ###   ########.fr       */
+/*   Updated: 2017/10/17 18:36:54 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,24 @@
 void	ft_dlsdelone(t_dlst **alst, void (*del)(void **))
 {
 	t_dlst	*tmp;
+	t_dlst	*nxt;
 
 	if (alst)
 	{
+		nxt = NULL;
 		tmp = *alst;
+		ft_putstr("deleting:");
+		if (tmp->cnt)
+ft_putendl((char*)tmp->cnt);
 		del(&tmp->cnt);
+		if (tmp->nxt)
+		{
+			nxt = tmp->nxt;
+			nxt->prv = tmp->prv;
+		}
+		if (tmp->prv)
+			tmp->prv = nxt;
 		ft_memdel((void**)alst);
-		*alst = NULL;
+		*alst = nxt;
 	}
 }
