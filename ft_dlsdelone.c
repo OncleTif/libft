794225065e3784a376e1e31f0858de9fff9924d3
tmanet <tmanet@student.42.fr>
2017-10-17 21:28:32 +0200

    dlsdelone ok

--- ft_dlsdelone.c
+++ ft_dlsdelone.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:52:34 by tmanet            #+#    #+#             */
-/*   Updated: 2017/10/17 18:36:54 by tmanet           ###   ########.fr       */
+/*   Updated: 2017/10/17 21:11:06 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,15 @@ void	ft_dlsdelone(t_dlst **alst, void (*del)(void **))
 	t_dlst	*tmp;
 	t_dlst	*nxt;
 
-	if (alst)
+	if (alst && *alst)
 	{
-		nxt = NULL;
 		tmp = *alst;
-		ft_putstr("deleting:");
-		if (tmp->cnt)
-ft_putendl((char*)tmp->cnt);
+		nxt = tmp->nxt;
 		del(&tmp->cnt);
 		if (tmp->nxt)
-		{
-			nxt = tmp->nxt;
 			nxt->prv = tmp->prv;
-		}
 		if (tmp->prv)
-			tmp->prv = nxt;
+			tmp->prv->nxt = nxt;
 		ft_memdel((void**)alst);
 		*alst = nxt;
 	}
--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 15:32:39 by tmanet            #+#    #+#             */
-/*   Updated: 2015/11/30 16:16:01 by tmanet           ###   ########.fr       */
+/*   Updated: 2017/10/17 20:05:46 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	length;
 
+	if (!s)
+		return (-1);
 	length = 0;
 	while (s[length])
 	{
